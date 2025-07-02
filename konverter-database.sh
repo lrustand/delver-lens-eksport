d9712d56bdc8bbfee7b16d2783dd718cad34cbb5
Lars Rustand <rustand.lars@gmail.com>
2025-07-02 14:25:50 +0200

    Mer verbose output fra skript

--- konverter-database.sh
+++ konverter-database.sh
@@ -6,3 +6,5 @@ sql_file="$1"
 out_file="${sql_file%.*}.txt"
 
 sqlite3 "$sql_file" -separator " " < delver.sql > "$out_file"
+
+echo "Kortliste er skrevet til $out_file"
--- synk.sh
+++ synk.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+echo "Finner siste backup-fil fra Delver Lens."
 db_navn="$(adb shell ls -1 --sort=time /storage/emulated/0/Download/*.dlens | head -n 1)"
 
+echo "Overfører backup-fil."
 adb pull "$db_navn"
 
+echo "Konverterer databasen til klartekst-format."
 ./konverter-database.sh "$(basename $db_navn)"
